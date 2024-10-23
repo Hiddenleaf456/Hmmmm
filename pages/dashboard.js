2517de32808f2492deb45abe68f5c0d756055e42
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-10-23 10:16:55 +0100

    Delete pages directory

--- pages/dashboard.js
+++
@@ -1,9 +0,0 @@
-
-export default function Dashboard() {
-    return (
-        <div>
-            <h1>Welcome to the Dashboard</h1>
-            <p>This is a secure area of the site.</p>
-        </div>
-    );
-}
