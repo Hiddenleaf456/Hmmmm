2456031f8d6569755fe3b596a5a7f52e6db82680
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-10-23 13:24:38 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,7 @@ export default function Dashboard() {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', height: '100vh', backgroundColor: '#f0f2f5', paddingTop: '50px' }}>
       <h1 style={{ fontSize: '4rem', color: '#0070f3' }}>Welcome To Toxxic API</h1>
+      
       <p style={{ fontSize: '1.5rem', marginTop: '20px', textAlign: 'center' }}>
         Access Our Free Unlimited APIs<br />
         To use GPT-3 API{' '}
@@ -12,12 +13,20 @@ export default function Dashboard() {
           <a style={{ color: '#0070f3', textDecoration: 'underline' }}>click here</a>
         </Link>
       </p>
+
       <p style={{ fontSize: '1.5rem', marginTop: '20px', textAlign: 'center' }}>
         To upload an image and get its URL{' '}
         <Link href="/api/upload-image">
           <a style={{ color: '#0070f3', textDecoration: 'underline' }}>click here</a>
         </Link>
       </p>
+
+      <p style={{ fontSize: '1.5rem', marginTop: '20px', textAlign: 'center' }}>
+        To download YouTube videos{' '}
+        <Link href="/api/ytdl?query=oseba">
+          <a style={{ color: '#0070f3', textDecoration: 'underline' }}>click here</a>
+        </Link>
+      </p>
     </div>
   );
-}
+          }
