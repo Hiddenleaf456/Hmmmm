b37c02bd84bf21b0f33be3329619a0a9517a67e5
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-10-23 10:04:09 +0100

    Delete pages directory

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '100px' }}>
-      <h1>Welcome to the API Site</h1>
-      <p>Go to <a href="/login">Login</a> or <a href="/dashboard">Dashboard</a>.</p>
-    </div>
-  );
-}
