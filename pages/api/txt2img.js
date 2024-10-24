0bc9e6f8f07778feffa7deae0dc30e399da14307
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-10-24 12:49:49 +0100

    Update txt2img.js

--- pages/api/txt2img.js
+++ pages/api/txt2img.js
@@ -1,29 +1,34 @@
+// /pages/api/some-endpoint.js
 import axios from 'axios';
+import { apiKeyMiddleware } from '../../lib/apiKeyMiddleware';
 
 export default async function handler(req, res) {
-    if (req.method === 'GET') {
-        const { prompt } = req.query;
+    // Call the API key middleware
+    apiKeyMiddleware(req, res, async () => {
+        if (req.method === 'GET') {
+            const { prompt } = req.query;
 
-        if (!prompt) {
-            return res.status(400).json({ message: 'Prompt is required' });
+            if (!prompt) {
+                return res.status(400).json({ message: 'Prompt is required' });
+            }
+
+            try {
+                const response = await axios.get(`https://itzpire.com/ai/photoleap?prompt=${encodeURIComponent(prompt)}`);
+
+                // Modify the response data to include "Toxxic" as the author
+                const modifiedData = {
+                    ...response.data,
+                    author: 'Toxxic'
+                };
+
+                res.status(200).json(modifiedData);
+            } catch (error) {
+                console.error(error);
+                res.status(500).json({ message: 'Error retrieving data' });
+            }
+        } else {
+            res.setHeader('Allow', ['GET']);
+            res.status(405).end(`Method ${req.method} Not Allowed`);
         }
-
-        try {
-            const response = await axios.get(`https://itzpire.com/ai/photoleap?prompt=${encodeURIComponent(prompt)}`);
-
-            // Modify the response data to include "Toxxic" as the author
-            const modifiedData = {
-                ...response.data,
-                author: 'Toxxic'
-            };
-
-            res.status(200).json(modifiedData);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: 'Error retrieving data' });
-        }
-    } else {
-        res.setHeader('Allow', ['GET']);
-        res.status(405).end(`Method ${req.method} Not Allowed`);
-    }
+    });
 }
