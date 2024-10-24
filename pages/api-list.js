a9cf6c14e4a344efe8a7538744bdd0a3e6c9ae9c
TOXXIC MD <161505113+Toxic1239@users.noreply.github.com>
2024-10-24 09:57:03 +0100

    Update api-list.js

--- pages/api-list.js
+++ pages/api-list.js
@@ -57,55 +57,7 @@ export default function ApiList() {
             </a>
           </Link>
 
-          {/** Upload Image API **/}
-          <Link href="/api/upload-image">
-            <a style={{
-              backgroundColor: '#00e676',
-              color: '#000',
-              padding: '15px 30px',
-              borderRadius: '10px',
-              fontSize: '1.3rem',
-              textDecoration: 'none',
-              textAlign: 'left',
-              transition: 'background-color 0.3s ease, transform 0.3s ease',
-              boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.3)',
-            }}
-            onMouseOver={e => {
-              e.target.style.backgroundColor = '#00c853';
-              e.target.style.transform = 'scale(1.05)';
-            }}
-            onMouseOut={e => {
-              e.target.style.backgroundColor = '#00e676';
-              e.target.style.transform = 'scale(1)';
-            }}>
-              IMG TO URL
-            </a>
-          </Link>
-
-{/** Upload Image API **/}
-          <Link href="/api/tts?text=Hello%20world">
-            <a style={{
-              backgroundColor: '#00e676',
-              color: '#000',
-              padding: '15px 30px',
-              borderRadius: '10px',
-              fontSize: '1.3rem',
-              textDecoration: 'none',
-              textAlign: 'left',
-              transition: 'background-color 0.3s ease, transform 0.3s ease',
-              boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.3)',
-            }}
-            onMouseOver={e => {
-              e.target.style.backgroundColor = '#00c853';
-              e.target.style.transform = 'scale(1.05)';
-            }}
-            onMouseOut={e => {
-              e.target.style.backgroundColor = '#00e676';
-              e.target.style.transform = 'scale(1)';
-            }}>
-              TTS EN
-            </a>
-          </Link>
+        
           {/** YouTube Download API **/}
           <Link href="/api/ytdl?query=oseba">
             <a style={{
