10a43f8422ae53ec115b10c185570dae09b00b1a
Hiddenleaf456 <redhearthacker15@gmail.com>
2024-10-29 07:13:18 +0100

    Update api-list.js

--- pages/api-list.js
+++ pages/api-list.js
@@ -201,8 +201,8 @@ export default function ApiList() {
               LYRICS API
             </a>
           </Link>
-                {/** LINES**/}
-          <Link href="/api/line?apikey=toxxicboy">
+                {/** TRANSLATE**/}
+          <Link href="/api/translate?text=hello&lang=en&apikey=toxxicboy">
             <a style={{
               backgroundColor: '#00e676',
               color: '#000',
@@ -223,6 +223,30 @@ export default function ApiList() {
               e.target.style.transform = 'scale(1)';
             }}>
               PICKUP LINES
+            </a>
+          </Link>
+                {/** LYRICS**/}
+          <Link href="/api/song?apikey=toxxicboy&song=believer">
+            <a style={{
+              backgroundColor: '#00e676',
+              color: '#000',
+              padding: '15px 30px',
+              borderRadius: '10px',
+              fontSize: '1.3rem',
+              textDecoration: 'none',
+              textAlign: 'left',
+              transition: 'background-color 0.3s ease, transform 0.3s ease',
+              boxShadow: '0px 4px 15px rgba(0, 0, 0, 0.3)',
+            }}
+            onMouseOver={e => {
+              e.target.style.backgroundColor = '#00c853';
+              e.target.style.transform = 'scale(1.05)';
+            }}
+            onMouseOut={e => {
+              e.target.style.backgroundColor = '#00e676';
+              e.target.style.transform = 'scale(1)';
+            }}>
+              TRANSLATE API
             </a>
           </Link>
         </div>
